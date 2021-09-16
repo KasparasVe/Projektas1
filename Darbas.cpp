317eb9c7939a9bfe16e926d78b7c6bba7cf6029f
KasparasVe <90028819+KasparasVe@users.noreply.github.com>
2021-09-16 20:39:45 +0300

    Atnaujinimas2

--- Darbas.cpp
+++ Darbas.cpp
@@ -14,16 +14,17 @@ using std::sort;
 
 struct studentas {
     string vardas, pavarde;
-    float nd[5]{};
+    int* nd;
     float egzam;
     float galutinisVid;
     float galutinisMed;
+    char skaiciavimas;
     
 };
 
 void pild(studentas& kint);
 void printas(studentas& kin);
-float mediana(float a[], int n);
+float mediana(int a[], int n);
 
 int main()
 {
@@ -39,41 +40,51 @@ int main()
     }
     for (int i = 0; i < k; i++)
         pild(studentai[i]);
-    cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(20) << left << "Galutinis(Vid.)" << setw(20) << left << "Galutinis(Med.)" << endl;
+    cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(20) << left << "Galutinis(Vid.)/Galutinis(Med.)"<< endl;
     for (int i = 0; i < k; i++)
         printas(studentai[i]);
 }
 
 void pild(studentas& kint) {
 
-    int n;
+    
+    
     float med;
     cout << "Iveskite studento varda ir pavarde: ";
     cin >> kint.vardas >> kint.pavarde;
-    cout << "Kiek nd bus?";
-    cin >> n;
+    
 
-    while (cin.fail()||n<0) {
-        cout << "Klaida! Iveskite SVEIKA skaiciu" << endl;
+    
+    
+    float sum = 0, vid = 0; 
+    int n = 0;
+    kint.nd = new int[10];
+    cout << "Iveskite pazymi, kai baigsite, iveskite bet kokia raide" << endl;
+    for (int i = 0; i < 10; i++)
+        if (cin >> kint.nd[i]) {
+            sum += kint.nd[i];
+            n++;
+        }
+        else { break; }
+    cin.clear();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    while (n == 0) { 
+        cout << "Klaida! Iveskite bent 1 pazymi!" << endl;
+        for (int i = 0; i < 10; i++)
+            if (cin >> kint.nd[i]) {
+                sum += kint.nd[i];
+                n++;
+            }
+            else { break; }
         cin.clear();
         cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        cin >> n;
-
     }
-    cout << "Iveskite pazymius: " << endl;
-    float sum = 0, vid = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> kint.nd[i];
-        while (cin.fail() || kint.nd[i] < 0 || kint.nd[i] > 10) {
-            cout << "Klaida! Iveskite TEIGIAMA SVEIKA skaiciu 1-10" << endl;
-            cin.clear();
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            cin >> kint.nd[i];
-        }
-        sum += kint.nd[i];
-    }
+        
+    
     vid = sum / n;
     med = mediana(kint.nd, n);
+    delete[] kint.nd;
+
     cout << "Iveskite egzamino bala: "; 
     cin >> kint.egzam;
     while (cin.fail() || kint.egzam < 0 || kint.egzam > 10) {
@@ -81,6 +92,14 @@ void pild(studentas& kint) {
         cin.clear();
         cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cin >> kint.egzam;
+    }
+    cout << "Spauskite v jei norite galutini bala skaiciuoti pagal vidurki ir m, jei pagal mediana: ";
+    cin >> kint.skaiciavimas;
+    while (cin.fail() || (kint.skaiciavimas != 'v' && kint.skaiciavimas != 'm') ){
+        cout << "Klaida! Iveskite raides v arba m" << endl;
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cin >> kint.skaiciavimas;
     }
 
     kint.galutinisVid = vid * 0.4 + 0.6 * kint.egzam;
@@ -91,10 +110,14 @@ void pild(studentas& kint) {
 
 }
 void printas(studentas &kin) {
-    
-    cout << setw(20) << left << kin.vardas << setw(20) << left << kin.pavarde << setw(20) << left << setprecision(3) << kin.galutinisVid << setw(20) << left << setprecision(3) << kin.galutinisMed << endl;
-}
-float mediana(float a[], int n) {
+
+    if (kin.skaiciavimas == 'v') {
+        cout << setw(20) << left << kin.vardas << setw(20) << left << kin.pavarde << setw(20) << left << setprecision(3) << kin.galutinisVid<< endl;
+    }
+    else
+        cout << setw(20) << left << kin.vardas << setw(20) << left << kin.pavarde << setw(20) << left << setprecision(3) << kin.galutinisMed << endl;
+    }
+float mediana(int a[], int n) {
     sort(a, a + n);
     if (n % 2 != 0)
         return (float)a[n / 2];
