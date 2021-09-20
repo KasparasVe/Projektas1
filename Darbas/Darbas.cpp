1e801c9d3bd309384aaa21f157132ee1836bc532
KasparasVe <90028819+KasparasVe@users.noreply.github.com>
2021-09-20 21:40:56 +0300

    Atnaujinimas 5. Versija v0.2

--- Darbas/Darbas.cpp
+++ Darbas/Darbas.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <fstream>
+#include <sstream> 
 
 using std::cout;
 using std::cin;
@@ -12,7 +14,10 @@ using std::setw;
 using std::left;
 using std::setprecision;
 using std::sort;
+using std::getline;
 using std::vector;
+using std::ifstream;
+using std::stringstream;
 
 struct studentas {
     string vardas, pavarde;
@@ -27,26 +32,49 @@ struct studentas {
 void pild(studentas& kint);
 void printas(studentas& kin);
 float mediana(vector<float> vec);
+void nuskaitymas(string read, char s);
+void uzpildymas(studentas& k, string eilute);
 
 int main()
 {
     studentas studentai[10];
     int k;
+    char answ, g;
+    string name;
     srand(time(NULL));
-
-    cout << "Iveskite studentu skaiciu: ";
-    cin >> k;
-    while (cin.fail() || k<0) {
-        cout << "Klaida! Iveskite TEIGIAMA SVEIKA skaiciu" << endl;
-        cin.clear();
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        cin >> k;   
+    
+    cout << "Ar norite duomenis nuskaityti is failo? (Jei taip, iveskite 't', jei ne - bet koki simboli): ";
+    cin >> answ;
+    if (answ == 't') {
+        cout << "Kaip skaiciuoti galutinius balus? (Iveskite 'v', jei pagal vidurki, 'm' - pagal mediana): ";
+        cin >> g;
+        while (cin.fail() || (g != 'v' && g != 'm')) {
+            cout << "Klaida! Iveskite raides 'v' arba 'm'" << endl;
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cin >> g;
+        }
+        cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(20) << left << "Galutinis(Vid.)/Galutinis(Med.)" << endl;
+        nuskaitymas("kursiokai.txt", g);
+        
+        
     }
-    for (int i = 0; i < k; i++)
-        pild(studentai[i]);
-    cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(20) << left << "Galutinis(Vid.)/Galutinis(Med.)"<< endl;
-    for (int i = 0; i < k; i++)
-        printas(studentai[i]);
+    else {
+        cout << "Iveskite studentu skaiciu: ";
+        cin >> k;
+        while (cin.fail() || k<0) {
+            cout << "Klaida! Iveskite TEIGIAMA SVEIKA skaiciu" << endl;
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cin >> k;
+        }
+        for (int i = 0; i < k; i++)
+            pild(studentai[i]);
+        cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(20) << left << "Galutinis(Vid.)/Galutinis(Med.)"<< endl;
+        for (int i = 0; i < k; i++)
+            printas(studentai[i]);
+    }
+ 
 }
 
 void pild(studentas& kint) {
@@ -136,11 +164,13 @@ void pild(studentas& kint) {
         }
         vid = sum / count;
         med = mediana(kint.nd);
+        
         kint.galutinisVid = vid * 0.4 + 0.6 * kint.egzam;
         kint.galutinisMed = med * 0.4 + 0.6 * kint.egzam;
     }
 }
 void printas(studentas &kin) {
+
 
     if (kin.skaiciavimas == 'v') {
         cout << setw(20) << left << kin.vardas << setw(20) << left << kin.pavarde << setw(20) << left << setprecision(3) << kin.galutinisVid<< endl;
@@ -158,3 +188,75 @@ float mediana(vector<float> vec) {
     vecSize vid = size / 2;
     return size % 2 == 0 ? (vec[vid] + vec[vid - 1]) / 2 : vec[vid];
 }
+
+void nuskaitymas(string read, char s) {
+    vector<string> sarasas;
+    string eil;
+    
+
+    ifstream failas(read);
+    while (failas) {
+        if (!failas.eof()) {
+            getline(failas, eil);
+            sarasas.push_back(eil);
+        }
+        else break;
+    }
+    failas.close();
+
+    vector<studentas> grupe;
+    
+    
+    for (int i = 0; i < sarasas.size()-1;i++) {
+        studentas tempas;
+        tempas.skaiciavimas = s;
+        uzpildymas(tempas, sarasas[i+1]);
+        grupe.push_back(tempas);
+        printas(grupe[i]);
+    }
+   
+
+    }
+    
+    
+
+void uzpildymas(studentas& k, string eilute) {
+    string str = eilute;
+    stringstream s(str);
+    float sum = 0, vid, med;
+
+
+    float nd1, nd2, nd3, nd4, nd5, nd6, nd7, nd8, nd9, nd10, nd11, nd12, nd13, nd14, nd15, egzam;
+    s >> k.vardas >> k.pavarde >> nd1 >> nd2 >> nd3 >> nd4 >> nd5 >> nd6 >> nd7 >> nd8 >> nd9 >> nd10 >> nd11 >> nd12 >> nd13 >> nd14 >> nd15 >> k.egzam;
+    k.nd.push_back(nd1);
+    k.nd.push_back(nd2);
+    k.nd.push_back(nd3);
+    k.nd.push_back(nd4);
+    k.nd.push_back(nd5);
+    k.nd.push_back(nd6);
+    k.nd.push_back(nd7);
+    k.nd.push_back(nd8);
+    k.nd.push_back(nd9);
+    k.nd.push_back(nd10);
+    k.nd.push_back(nd11);
+    k.nd.push_back(nd12);
+    k.nd.push_back(nd13);
+    k.nd.push_back(nd14);
+    k.nd.push_back(nd15);
+
+    for (float p : k.nd) {
+        sum += p;
+    }
+
+
+    vid = sum / k.nd.size();
+    med = mediana(k.nd);
+    
+    k.galutinisVid = 0.4 * vid + 0.6 * k.egzam;
+    k.galutinisMed = med * 0.4 + 0.6 * k.egzam;
+    
+}
+
+    
+
+    
